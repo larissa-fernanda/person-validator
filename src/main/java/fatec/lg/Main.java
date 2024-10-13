248c3e9277612631da80bcb5f159a8478dee770d
larissa-fernanda <laliffr@gmail.com>
2024-10-13 15:33:25 -0300

    feat: Adiciona implementação da classe main

--- src/main/java/fatec/lg/Main.java
+++ src/main/java/fatec/lg/Main.java
@@ -1,7 +1,21 @@
 package fatec.lg;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Email> emails = new ArrayList<Email>();
+
+        Email email = new Email(1, "fulaninho.silva@email.com");
+        emails.add(email);
+
+        Person person = new Person(1, "Fulaninho Silva", 25, emails);
+        PersonDAO personDAO = new PersonDAO();
+
+        Email email2 = new Email(2, "fulaninhosilva.contato@email.com");
+        person.addEmail(email2);
+
+        personDAO.save(person);
     }
 }
